ef61019d3d44286b974e5055ea1238a8e5664bab
denise baker <denviol@yahoo.com>
2020-02-29 13:31:50 -0600

    Add project files.

---
+++ value masking c sharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace value_masking_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret;
+
+            Console.WriteLine("Ahoy Matey!  Tell me a secret...");
+            secret = Console.ReadLine();
+            if (secret.Length > 4)
+            {
+                Console.WriteLine(
+                    string.Concat(
+                        "".PadLeft(12, '*'),
+                        secret.Substring(secret.Length - 4)
+                    )
+                );
+            }
+            else
+            {
+                Console.WriteLine(secret);
+            }
+        }
+    }
+}
